a366443f227733b93b965fcc87915609f27d237a
Aditya <shinde.aditya692@gmail.com>
2024-11-02 18:31:00 +0530

    mapper_and_repo

--- src/main/java/com/example/myproject/mapper/CustomerMapper.java
+++ src/main/java/com/example/myproject/mapper/CustomerMapper.java
@@ -1,4 +1,32 @@
 package com.example.myproject.mapper;
 
+import com.example.myproject.dto.CustomerRequest;
+import com.example.myproject.dto.CustomerResponse;
+import com.example.myproject.entity.Customer;
+import org.springframework.stereotype.Service;
+@Service
+
 public class CustomerMapper {
+    public Customer toEntity(CustomerRequest request) {
+        return Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .password(request.password())
+                .address(request.address())
+                .city(request.city())
+                .pincode(request.pincode())
+                .build();
+    }
+
+    public CustomerResponse toCustomerResponse(Customer customer) {
+        return new CustomerResponse(
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getAddress(),
+                customer.getCity(),
+                customer.getPincode()
+        );
+    }
 }
--- src/main/java/com/example/myproject/repo/CustomerRepo.java
+++ src/main/java/com/example/myproject/repo/CustomerRepo.java
@@ -1,4 +1,7 @@
 package com.example.myproject.repo;
 
+import com.example.myproject.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface CustomerRepo {
 }
