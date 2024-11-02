66eed643b63c386ce5f855b35bcc514bdd682596
Aditya <shinde.aditya692@gmail.com>
2024-11-02 18:24:25 +0530

    controller_and_service

--- src/main/java/com/example/myproject/controller/CustomerController.java
+++ src/main/java/com/example/myproject/controller/CustomerController.java
@@ -1,4 +1,25 @@
 package com.example.myproject.controller;
 
+import com.example.myproject.dto.CustomerRequest;
+import com.example.myproject.dto.CustomerResponse;
+import com.example.myproject.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/customer")
+
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerResponse> createCustomer(@RequestBody @Valid CustomerRequest request) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
 }
--- src/main/java/com/example/myproject/service/CustomerService.java
+++ src/main/java/com/example/myproject/service/CustomerService.java
@@ -1,4 +1,25 @@
 package com.example.myproject.service;
 
+import com.example.myproject.dto.CustomerRequest;
+import com.example.myproject.dto.CustomerResponse;
+import com.example.myproject.entity.Customer;
+import com.example.myproject.mapper.CustomerMapper;
+import com.example.myproject.repo.CustomerRepo;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepo repo;
+    private final CustomerMapper mapper;
+    public CustomerResponse createCustomer(CustomerRequest request) {
+        Customer customer = mapper.toEntity(request);
+        Customer cust = repo.save(customer);
+        return mapper.toCustomerResponse(cust);
+    }
 }
+
